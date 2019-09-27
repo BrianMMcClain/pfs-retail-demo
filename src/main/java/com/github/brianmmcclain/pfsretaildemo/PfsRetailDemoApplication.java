0a8e951a9876ad4ceb227ce850e87c86dbf04456
Brian McClain <brianmmcclain@gmail.com>
2019-09-27 09:24:35 -0400

    Initial commit

---
+++ src/main/java/com/github/brianmmcclain/pfsretaildemo/PfsRetailDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.brianmmcclain.pfsretaildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfsRetailDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfsRetailDemoApplication.class, args);
+	}
+
+}
